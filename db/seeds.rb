3a12d692eba4c91f9435c88b1c45380daf66aa79
Adam La Rosa <adam.larosa@flatironschool.com>
2021-12-22 12:55:35 -0800

    solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
+
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
 Company.create(name: "Enron", founding_year: 1995)
+Company.create(name: "Dunder Mifflin", founding_year: 2002)
+
 
 puts "Creating devs..."
 Dev.create(name: "Rick")
@@ -17,5 +20,29 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+adam = Dev.create(name: 'Adam')
+emiley = Dev.create(name: 'Emiley')
+
+Freebie.create(
+        dev: emiley,
+        company_id: Company.first,
+        item_name: 'road bike',
+        value: 1000
+    )
+Freebie.create(
+    dev: adam,
+    company_id: Company.first,
+    item_name: 'road bike',
+    value: 1000
+)
+
+10.times do
+    Freebie.create(
+        dev_id: Dev.all.sample.id,
+        company_id: Company.all.sample.id,
+        item_name: Faker::Vehicle.make,
+        value: (rand * 100000).to_i
+    )
+end
 
 puts "Seeding done!"
