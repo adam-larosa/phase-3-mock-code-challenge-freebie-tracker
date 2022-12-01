d7fb831aefdade6f3d2b18c1aaa7d829109d00e4
Adam La Rosa <adam.larosa@flatironschool.com>
2022-12-01 10:10:31 -0800

    101022 solution

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,23 @@
 class Company < ActiveRecord::Base
+    has_many :freebies 
+    has_many :devs, through: :freebies 
 
+    def give_freebie( dev_instance, new_item_name, value_given ) 
+        # takes a dev (an instance of the Dev class), an item_name (string), 
+        # and a value (integer) as arguments, and creates a new Freebie instance 
+        # associated with this company and the given dev
+        Freebie.create( 
+            item_name: new_item_name, 
+            value: value_given, 
+            dev_id: dev_instance.id,
+            company_id: self.id
+        )
+    end
+
+    def self.oldest_company
+        # returns the Company instance with the earliest founding year
+
+        # self.all.min_by {|c| c.founding_year }
+        self.order( :founding_year ).first
+    end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,33 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        # accepts an item_name (string) and returns true if any of the 
+        # freebies associated with the dev has that item_name, otherwise 
+        # returns false
+
+        # self.freebies.each do |freebie|
+        #     if item_name == freebie.item_name
+        #         return true
+        #     end
+        # end
+        # return false
+        
+        # !!self.freebies.find_by( item_name: item_name )
+
+        self.freebies.find_by(item_name: item_name) ? true : false
+    end
+
+    def give_away(dev_to_get_freebie, freebie)
+        # accepts a Dev instance and a Freebie instance, changes the freebie's 
+        # dev to be the given dev; your code should only make the change if 
+        # the freebie belongs to the dev who's giving it away
+        if self.id == freebie.dev.id
+            freebie.update( dev_id: dev_to_get_freebie.id )
+        else
+            puts 'DO NOT GIVE WHAT IS NOT YOURS!'
+        end
+    end
 
 end
